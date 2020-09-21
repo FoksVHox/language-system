5caa526e2c8ba92ddb6af779da5c112f3a04be65
Foks <FoksVHox@users.noreply.github.com>
2020-09-21 19:43:53 +0000

    Apply fixes from StyleCI

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,17 +2,16 @@
 
 namespace FoksVHox\LanguageSystem\Tests;
 
-use Orchestra\Testbench\TestCase;
 use FoksVHox\LanguageSystem\LanguageSystemServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     protected function getPackageProviders($app)
     {
         return [LanguageSystemServiceProvider::class];
     }
-    
+
     /** @test */
     public function true_is_true()
     {
